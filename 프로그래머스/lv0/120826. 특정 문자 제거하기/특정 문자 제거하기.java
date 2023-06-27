a68c706014ee376e2a505b0b334ae13322bbedb8
yamwoong <100250784+yamwoong@users.noreply.github.com>
2023-06-27 14:01:26 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.07 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        
+        answer = my_string.replaceAll(letter, "");
+        
+        return answer;
+    }
+}
