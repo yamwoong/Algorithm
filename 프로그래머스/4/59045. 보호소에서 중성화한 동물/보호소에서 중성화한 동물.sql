5117eca65cb1e7641454f34e3a151028382de15e
yamwoong <100250784+yamwoong@users.noreply.github.com>
2024-04-27 20:16:51 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,10 @@
+SELECT  AI.ANIMAL_ID
+     ,  AI.ANIMAL_TYPE
+     ,  AI.NAME
+  FROM  ANIMAL_INS  AI
+     ,  ANIMAL_OUTS AO
+ WHERE  AI.ANIMAL_ID    = AO.ANIMAL_ID
+   AND  AI.SEX_UPON_INTAKE  IN ('Intact Male'  , 'Intact Female')
+   AND  AO.SEX_UPON_OUTCOME IN ('Neutered Male', 'Spayed Female')
+ ORDER BY AI.ANIMAL_ID
+;
